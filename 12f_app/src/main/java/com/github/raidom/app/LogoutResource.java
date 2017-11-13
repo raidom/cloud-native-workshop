c5c8cbf6dd2a708bd38e57c4f6e815dd1971c280
Raido Mägi <raido.magi@smit.ee>
2017-11-13 15:11:24 +0200

    security

---
+++ 12f_app/src/main/java/com/github/raidom/app/LogoutResource.java
@@ -0,0 +1,15 @@
+package main.java.com.github.raidom.app;
+
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class LogoutResource {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpSession session) {
+
+        session.invalidate();
+        return "Logout done!";
+    }
+}
---
+++ 12f_app/src/main/java/com/github/raidom/app/SecuredResource.java
@@ -0,0 +1,21 @@
+package main.java.com.github.raidom.app;
+
+import org.springframework.web.bind.annotation.*;
+import java.security.Principal;
+
+@RestController
+@RequestMapping(value = "/secured")
+public class SecuredResource {
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String userHello(Principal principal) {
+
+        return "Hello " + principal.getName() + "!";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminHello(Principal principal) {
+
+        return "Hello " + principal.getName() + "!";
+    }
+}
